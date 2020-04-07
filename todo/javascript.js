cc97d7b9ebf748c28ed021d3304e81996395e0cc
jaelolpark <ac2cic@gmail.com>
2020-04-07 17:45:37 -0400

    to do

--- todo/javascript.js
+++ todo/javascript.js
@@ -5,12 +5,36 @@ let input = prompt("What would you like to do?");
   
   while(input !== "quit") {
     if(input === 'list') {
-      console.log(todos);
+      listTodo();
     } else if(input === 'new') {
-    let newTodo = prompt("Enter new Todo");
-      todos.push(newTodo);
+      addTodo();
+    } else if(input === 'delete') {
+      deleteTodo();
     }
-    // quit이면 바로 여기루
+    // 새로운 todo를 만들면 다시
     input = prompt("What would you like to do?");
   }
+  // quit이 되면 여기로 오고 끝.
 console.log("OK, You quit the app");
+
+// ===========================================
+
+function listTodo() {
+  console.log("***********");
+    todos.forEach(function(todo, i) {
+      console.log(i + ": " + todo);
+    });
+    console.log("***********");
+}
+
+function addTodo() {
+  let newTodo = prompt("Enter new Todo");
+      todos.push(newTodo);
+      console.log("Added new todo");
+}
+
+function deleteTodo() {
+  let index = prompt("Enter index of todo to delete");
+      todos.splice(index, 1);
+      console.log("Deleted todo");
+}
